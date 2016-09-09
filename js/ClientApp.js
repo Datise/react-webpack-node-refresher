ce13c488d9892e862652bcb6c90a788aec7188d8
Datise <donald.ballard@hotmail.com>
2016-09-09 13:49:57 -0700

    client app comment to test github changes

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,7 +1,7 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var MyTitle = require("./MyTitle")
-
+// THE FUTURE IS NIGH
 var div = React.DOM.div
 var MyTitleFactory = React.createFactory(MyTitle)
 var MyFirstComponent = (
