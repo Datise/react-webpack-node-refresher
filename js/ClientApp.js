d83e32bde9094af6e708dc2a4fd3e1473bd63293
Datise <donald.ballard@hotmail.com>
2016-09-09 13:40:54 -0700

    switch entirely to webpack build

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,13 +1,8 @@
-/* global React and ReactDOM */
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require("./MyTitle")
+
 var div = React.DOM.div
-var h1 = React.DOM.h1
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null, h1(null, this.props.title))
-    )
-  }
-})
 var MyTitleFactory = React.createFactory(MyTitle)
 var MyFirstComponent = (
   div(null,
